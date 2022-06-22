74f90f8ba851a4814826f6c3a0cfd7632d006a9f
lc <lightingcove@gmail.com>
2022-06-23 00:29:50 +0800

    added

--- src/pages/gamble.vue
+++ src/pages/gamble.vue
@@ -2,7 +2,7 @@
   <div class="lg:container lg:mx-auto pt-10 md:pt-12">
     <BalStack vertical>
       <div class="px-4 lg:px-0">
-        <h3 class="mb-3">{{ $t('investmentPools') }}</h3>
+        <h3 class="mb-3">{{ $t('gamblepools') }}</h3>
       </div>
     </BalStack>
   </div>
