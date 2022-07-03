385b6a5bfe05b2b1f381fcd1960b5fad754f81ff
lc <lightingcove@gmail.com>
2022-07-04 00:11:49 +0800

    Update GambleCard.vue

---
+++ src/components/cards/GambleCard/GambleCard.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { computed, defineComponent, ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const title = computed(() => {
+	if ('expired') return t('passed');
+	if ('upcoming') return t('upcoming');
+	if ('after') return t('after');
+	return t('onair');
+});
+</script>
+
+<template>
+    <BalCard>
+		<template v-slot:header>
+			<div class="w-full flex items-center justify-between">
+				<h4 class="font-bold">{{ title }}</h4>
+			</div>
+		</template>
+		<div>
+			
+		</div>
+    </BalCard>
+</template>
